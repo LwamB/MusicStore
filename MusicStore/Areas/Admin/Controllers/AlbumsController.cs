3221f9758089c9dd17579f2a350109d94b6bceba
LwamB <Lwamberihu0@gmail.com>
2023-05-28 19:17:20 +0200

    Part-1 is completed.

---
+++ MusicStore/Areas/Admin/Controllers/AlbumsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public class AlbumsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
